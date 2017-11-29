3849a4e87543eb280dc1be0cb18e5a6916270d56
CK <chris.kelada@gmail.com>
2017-11-29 21:16:20 +1100

    resolved authentication issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     before_action :authenticate_user!
-    helper_method :user_signed_in?, :current_user
+##    helper_method :user_signed_in?, :current_user
     
 #    protected
     
@@ -16,13 +16,4 @@ class ApplicationController < ActionController::Base
 #    def user_signed_in?
 #      current_user.present?
 #    end
-
-    private
-
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-
-    end
-    
-    helper_method :current_user
- end
+end
