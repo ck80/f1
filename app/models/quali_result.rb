6276a972e36f79bc50bc71b2ebe089af07af3ade
~ck~ <chris.kelada@gmail.com>
2017-12-10 12:33:11 +1100

    added uniqueness for results and updated nav

--- app/models/quali_result.rb
+++ app/models/quali_result.rb
@@ -1,4 +1,6 @@
 class QualiResult < ApplicationRecord
   belongs_to :race
   belongs_to :driver
+  validates :position, :race_id, :driver_id, presence: true
+  validates :position, uniqueness: { scope: [:race_id, :driver_id], message: ": you already have an entry for this race, please edit the existing tip instread of create a new one." }
 end
