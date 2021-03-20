b02b4f2633786a7e56b4d7d00817768b2bb68f4b
~ck~ <chris.kelada@gmail.com>
2021-03-21 00:16:35 +1100

    platform upgrade plus update for season 2021

--- app/controllers/race_results_controller.rb
+++ app/controllers/race_results_controller.rb
@@ -88,7 +88,7 @@ class RaceResultsController < ApplicationController
     seasonstartid = 958 # first race id less 1 to allow for adding raceno to build url
     raceid = seasonstartid + Race.where(country: @country).pluck(:race_number).first
     page = "https://www.formula1.com/en/results.html/#{@year}/races/#{raceid}/#{@country.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')}/race-result.html"
-    doc = Nokogiri::HTML(open(page))   
+    doc = Nokogiri::HTML(URI.open(page))   
     
     table=doc.css('table.resultsarchive-table')
     rows=table.css('tr')
