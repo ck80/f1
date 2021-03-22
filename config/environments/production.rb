4b99e83ca31e38643dbd92795dafb2fb9cf4da29
~ck~ <chris.kelada@gmail.com>
2021-03-22 22:41:51 +1100

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,24 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # setting up mailer config
+  config.action_mailer.default_url_options = { :host => ENV["WEBSITE_DOMAIN"] }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+    }
+    
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
